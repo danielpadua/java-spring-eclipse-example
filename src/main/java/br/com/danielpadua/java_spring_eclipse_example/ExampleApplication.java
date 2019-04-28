76a7dc4d805968105f333c617bafb352f9fa61b6
danielpadua <daniel.padua@outlook.com>
2019-04-28 01:10:32 -0300

    Initial commit

---
+++ src/main/java/br/com/danielpadua/java_spring_eclipse_example/ExampleApplication.java
@@ -0,0 +1,13 @@
+package br.com.danielpadua.java_spring_eclipse_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleApplication.class, args);
+	}
+
+}
